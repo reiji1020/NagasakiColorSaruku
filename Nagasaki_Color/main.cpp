6335e68b178fd86226e2b323b99cb987da71afde
Saya Katafuchi <sk@reiji1020.info>
2015-11-02 00:07:12 +0900

    URL追加とか画面サイズ固定とか

--- Nagasaki_Color/main.cpp
+++ Nagasaki_Color/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <windows.h>
+#include <shellapi.h>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/opencv_lib.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+#pragma comment(lib, "shell32.lib")
 
 using namespace cv;
 using namespace std;
@@ -10,12 +14,14 @@ using namespace std;
 int main(void) {
 
 	uchar hue, sat, val;
-	Mat src_video;
-	Mat smooth_video;
-	Mat hsv_img;
-	Mat hsv_video;
-	Mat frame;
+	bool browser_flag = false;
+	Mat src_video(Size(640,480),CV_8UC1,Scalar::all(255));
+	Mat smooth_video(Size(640, 480), CV_8UC1, Scalar::all(255));
+	Mat dst_img(Size(640, 480), CV_8UC1, Scalar::all(0));
+	Mat hsv_video(Size(640, 480), CV_8UC1, Scalar::all(255));
+	Mat frame(Size(640, 480), CV_8UC1, Scalar::all(255));
 	VideoCapture capture(0);
+
 	// カメラが使えない場合はプログラムを止める
 	if (!capture.isOpened())
 		return -1;
@@ -28,16 +34,15 @@ int main(void) {
 	// 何かキーが押下されるまで、ループをくり返す
 	while (cvWaitKey(1) == -1)
 	{
-		hsv_img = Scalar(0, 0, 0);
+		dst_img = Scalar::all(0);
 		// カメラから1フレーム取得する
 		capture >> frame;
 		src_video = frame;
-		hsv_img = Mat(Size(src_video.cols, src_video.rows), CV_8UC1);
 		imshow(windowName,src_video);
 
 		// HSV表色系へ色情報を変換
 		// 先にノイズを消しておく
-		medianBlur(src_video, smooth_video, 7);
+		medianBlur(src_video, smooth_video, 5);
 		cvtColor(smooth_video, hsv_video, CV_BGR2HSV);
 		//imshow(hsv_window, hsv_video);
 
@@ -48,16 +53,19 @@ int main(void) {
 				sat = hsv_video.at<Vec3b>(y, x)[1];
 				val = hsv_video.at<Vec3b>(y, x)[2];
 				// 居留地マップの検出
-				if ((hue < 35 && hue > 20) && sat > 100) {
-					hsv_img.at<uchar>(y, x) = 255;
+				if ((hue < 35 && hue > 20) && sat > 127) {
+					dst_img.at<uchar>(y, x) = 255;
+					browser_flag = true;
 				}
 				else {
-					hsv_img.at<uchar>(y, x) = 0;
+					dst_img.at<uchar>(y, x) = 0;
 				}
 			}
 		}
-		imshow("認識結果",hsv_img);
+		imshow("認識結果",dst_img);
 	}
+	if (browser_flag = true)
+		ShellExecute(0, 0, L"http://www.saruku.info/", 0, 0, SW_SHOW);
 	destroyAllWindows();
 	return 0;
 }
